d029fcc56791929039bed579ef1884bdc817f3a6
Ganesh Tiwari <ganesht049@gmail.com>
2024-09-19 00:17:00 +0530

    feat: create a list of total usd credits

--- src/main.py
+++ src/main.py
@@ -15,15 +15,36 @@ def process_statement():
   # logger.info(len(empty_value))
   # logger.info(empty_value == " ") # <- returned true
 
-  statement["DR"] = statement["DR"].fillna(0).astype(int)
-  statement["CR"] = statement["CR"].fillna(0).astype(int)
+  statement["CR"] = statement["CR"].str.strip()
+  statement["DR"] = statement["DR"].str.strip()
+
+  statement["DR"] = statement["DR"].replace(to_replace="",value="0.00")
+  statement["CR"] = statement["CR"].replace(to_replace="",value="0.00")
+
+  statement["DR"] = statement["DR"].astype(float).astype(int)
+  statement["CR"] = statement["CR"].astype(float).astype(int)
+
   credit_records = statement[statement["DR"] == 0]
   debit_records  = statement[statement["CR"] == 0]
 
-  
-  logger.info(credit_records,debit_records)  
+  # logger.info(credit_records)
+  # logger.info(debit_records)
 
-  # logger.info(total_credits, total_debits)
+  total_credits = credit_records["CR"].sum()
+  total_debits  = debit_records["DR"].sum()
+
+  logger.info(f"Total Credits: {total_credits}")
+  logger.info(f"Total Debits: {total_debits}")
+
+  # credits which have USD in their PARTICULARS column
+  usd_credits = credit_records[credit_records["PARTICULARS"].str.contains("USD")]
+  usd_credits_total = usd_credits["CR"].sum()
+
+  # Print each USD credit record with Tran Date, BAL and SOL
+  for index, row in usd_credits.iterrows():
+    print(f"{index}: {row["Tran Date"]}\t{row["PARTICULARS"]}\t{row["CR"]}\t{row["BAL"]}\t{row["SOL"]}")
+  logger.info(f"USD Credits: {usd_credits_total}")
+
   
 if __name__ == "__main__":
   process_statement()
